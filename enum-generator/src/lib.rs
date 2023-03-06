b9067c292631976b41d130a0ba8e1fa0f856cbd0
Francis Nixon <13nixonf@gmail.com>
2023-03-05 21:49:43 -0500

    write giant match over all instructions and instruction encodings.

--- enum-generator/src/lib.rs
+++ enum-generator/src/lib.rs
@@ -6,22 +6,22 @@ use std::num::NonZeroU8;
 
 use itertools::Itertools;
 
-use wrapper_common::instructions::{InstructionEncoding, Instructions};
+use wrapper_common::instructions::{InstructionEncoding, InstructionName, Instructions};
 use wrapper_common::operand_index::OperandIndex;
 
 #[proc_macro]
-pub fn make_enum(_item: TokenStream) -> TokenStream {
+pub fn make_enums(_item: TokenStream) -> TokenStream {
     let Instructions { instructions } = get_instruction_metadata();
     let mut res = vec![];
-    for (instruction_name, instruct) in instructions {
-        let mut enum_ = "".to_string();
-        enum_.push_str("pub enum ");
-        enum_.push_str(instruction_name.0.replace("{evex} ", "vex").replace("REPE ", "repe").as_str());
-        enum_.push_str(" { ");
-        enum_.push_str(instruct.encodings.iter().map(|encoding| {
+    for (instruction_name, instruct) in instructions.iter() {
+        let mut instruction_encoding_enum = "".to_string();
+        instruction_encoding_enum.push_str("pub enum ");
+        instruction_encoding_enum.push_str(instruction_name_to_rust_enum_name(&instruction_name).as_str());
+        instruction_encoding_enum.push_str(" { ");
+        instruction_encoding_enum.push_str(instruct.encodings.iter().map(|encoding| {
             let mut operand_types = vec![];
             let InstructionEncoding { zeroing, bcast, iform:_, mode_prefix_string, operands } = encoding;
-            let prefix = "".to_string() /*+ dbg!(iform.as_str())*/ + mode_prefix_string.as_str() + if *zeroing{
+            let prefix = mode_prefix_string.to_string() + if *zeroing{
                 "Zeroing"
             }else {
                 ""
@@ -43,11 +43,24 @@ pub fn make_enum(_item: TokenStream) -> TokenStream {
                 operand_types.iter().map(|operand_type| operand_type.to_identifier_string()).join("_")
             }.as_str()
         }).join(",\n ").as_str());
-        enum_.push_str(" } ");
-        let token_stream: TokenStream = enum_.parse().unwrap();
+        instruction_encoding_enum.push_str(" } ");
+        let token_stream: TokenStream = instruction_encoding_enum.parse().unwrap();
         res.extend(token_stream);
     }
+    let mut instruction_enum = "pub enum Instructions {".to_string();
+    for (instruction_name, instruction) in instructions.iter() {
+        let variant_name = instruction_name_to_rust_enum_name(instruction_name);
+        instruction_enum.push_str(format!("{}({}),", variant_name, variant_name).as_str());
+    }
+    instruction_enum.push_str("}");
+    let token_stream: TokenStream = instruction_enum.parse().unwrap();
+    res.extend(token_stream);
+
     TokenStream::from_iter(res.into_iter())
+}
+
+fn instruction_name_to_rust_enum_name(instruction_name: &InstructionName) -> String {
+    instruction_name.0.replace("{evex} ", "vex").replace("REPE ", "repe")
 }
 
 fn get_instruction_metadata() -> Instructions {
